7d331b85717a2ab4909a4af9d72cdcddad2fc1e8
jaygal <jaygal@Joshuas-MBP.lan>
2021-09-27 21:37:11 +0200

    starting develop tree

--- pages/index.js
+++ pages/index.js
@@ -20,52 +20,52 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Mantra</title>
         <meta name="description" content="mantra" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&family=Libre+Baskerville&display=swap"
           rel="stylesheet"
         />
       </Head>
+      {/* HEADER */}
       <header className={styles.header}>
         <div className={styles.logo}>commonplace book</div>
         <div className={styles.link}>about</div>
       </header>
-
+      {/* MAIN */}
       <main className={styles.main}>
         <div className={styles.headingWrapper}>
           <h1 className={styles.h1}>{data[book].highlights[passage].text}</h1>
         </div>
-        <div className={styles.sidebar}>
-          <button className={styles.button} onClick={getQuote}>
-            New Note
-          </button>
-          <div>
-            <div className={styles.wrapper}>
-              <h2 className={styles.h2}>title</h2>
-              <p className={styles.text}>{data[book].title}</p>
-            </div>
-            <div className={styles.wrapper}>
-              <h2 className={styles.h2}>author</h2>
-              <p className={styles.text}>{data[book].authors}</p>
-            </div>
-            <div className={styles.wrapper}>
-              <h2 className={styles.h2}>Location</h2>
-              <p className={styles.text}>
-                {data[book].highlights[passage].location.value}
-              </p>
-            </div>
+      </main>
+      {/* SIDEBAR */}
+      <div className={styles.sidebar}>
+        <button className={styles.button} onClick={getQuote}>
+          New Note
+        </button>
+        <div>
+          <div className={styles.wrapper}>
+            <h2 className={styles.h2}>title</h2>
+            <p className={styles.text}>{data[book].title}</p>
           </div>
-          <div className={styles.footer}>
-            <div>built by joshua galinato</div>
+          <div className={styles.wrapper}>
+            <h2 className={styles.h2}>author</h2>
+            <p className={styles.text}>{data[book].authors}</p>
+          </div>
+          <div className={styles.wrapper}>
+            <h2 className={styles.h2}>Location</h2>
+            <p className={styles.text}>
+              {data[book].highlights[passage].location.value}
+            </p>
           </div>
         </div>
-      </main>
-    </div>
+        <footer className={styles.footer}>built by joshua galinato</footer>
+      </div>
+    </>
   )
 }
